24ec4f24d404117bccbaa8fa51450dc1732e4a92
Emanuel Castro <emanuel.castro@gmail.com>
2018-11-27 15:39:25 -0300

    commit teste

--- cursomc/src/main/java/com/udemy/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/udemy/cursomc/CursomcApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication {
 
+	// Teste GIT
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
