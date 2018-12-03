1e19d1fce353c2ebf5c3855b67f67f094997d459
Emanuel Castro <emanuel.castro@gmail.com>
2018-12-03 17:28:57 -0300

    Commit até a aula 34

--- cursomc/src/main/java/com/udemy/cursomc/services/CategoriaService.java
+++ cursomc/src/main/java/com/udemy/cursomc/services/CategoriaService.java
@@ -1,12 +1,15 @@
 package com.udemy.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.udemy.cursomc.domain.Categoria;
 import com.udemy.cursomc.repositories.CategoriaRepository;
+import com.udemy.cursomc.services.exceptions.DataIntegrityException;
 import com.udemy.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,9 +18,32 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);		
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que tem produtos.");
+		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
---
+++ cursomc/src/main/java/com/udemy/cursomc/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,18 @@
+package com.udemy.cursomc.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException{
+
+	/**
+	 * Serial.
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DataIntegrityException(String msg) {
+		super(msg);
+	}
+	
+	public DataIntegrityException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
