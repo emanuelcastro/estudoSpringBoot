70780880978dd3256d29744c90be347e5502f9c2
Emanuel Castro <emanuel.castro@gmail.com>
2018-12-18 10:07:57 -0300

    Commit Atualização Notebook

--- cursomc/src/main/java/com/udemy/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/udemy/cursomc/CursomcApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
-	// Teste GIT
+	// Teste
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
